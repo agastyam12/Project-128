3dd9838ea9833ae5de7e72799b83b6760bc9abba
Agastya Mishra <83354866+agastyam12@users.noreply.github.com>
2022-02-25 16:34:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+song1 = "";
+song2 = "";
+leftwristX = 0;
+leftwristY = 0;
+rightwristX = 0;
+rightwristY = 0;
+function preload(){
+  song1 = loadSound("music.mp3");
+  song2 = loadSound("music2.mp3");
+}
+function setup(){
+    poseNet = ml5.poseNet(video , modelLoaded);
+  poseNet.on('pose' , gotPoses);
+}
+    function modelLoaded(){
+        console.log('PoseNet is Initialized');
+    }
+
+ function draw() {
+     image(video , 0 , 0 , 600 , 500);
+ }
+
+function play(){
+    song.play();
+}
+function gotPoses(results){
+  if(results.length > 0){
+      console.log(results);
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX = "+leftWristX + "leftWristY = "+leftWristY);
+
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX = "+rightWristX + "rightWristY = "+rightWristY);
+  }
+}
